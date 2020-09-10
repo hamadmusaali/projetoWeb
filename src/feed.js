b30fef272caf390d175c07758521ced48684f3b6
allan-silveira <54253262+allan-silveira@users.noreply.github.com>
2020-09-10 17:14:17 -0300

    Implementação de busca através da API open brewery db

--- src/feed.js
+++ src/feed.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import $ from "jquery";
 import axios from "axios";
+import './style.css';
 
 function Buscar(){
     var busca = document.getElementById("busca"),
         ul = document.querySelector('ul');
-    axios.get('https://api.openbrewerydb.org/breweries')
+    axios.get('https://api.openbrewerydb.org/breweries?by_name=' + busca.value)
         .then(function(res){
             console.log(res);
             var docs = res.data;
+            ul.innerHTML = ""; 
             for(var i = 0; i < docs.length && i<10; i++){
                 var li = document.createElement('li');
-                li.innerHTML = docs[i].name;
+                li.innerHTML = "Nome do Estabelecimento: " +docs[i].name+"<br>Endereço: "
+                                +docs[i].street+", "+docs[i].city+"<br>Site: <a href=" 
+                                + docs[i].website_url +">" +  docs[i].website_url + "</a><br><br>";
                 ul.appendChild(li);
             }
+
         });
 }
 
 function Feed() {
 
     return (
-        <div>
+        <div className="container">
             <h1>Cervejarias</h1>
-            <input type="text" id="busca" value=""></input>
+            <input type="text" id="busca"></input>
             <button onClick={Buscar}>Buscar</button>
             <ul>
             </ul>
